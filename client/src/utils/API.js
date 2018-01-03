23692978015049c72ceae2cd3da4131cee8678c9
vin <vrp908@gmail.com>
2018-01-03 14:50:12 -0500

    C:/Program Files/Git/

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,11 +10,11 @@ export default {
  
   // get boxes/forums(main page) ######MAIN
  
- // create box/forum             ######MAIN
+ // create box/forum             ######MAIN/USERCREATE
 
   // get posts from individual box page      ######BOX
 
-  // create post for individual box page     ####### BOX
+  // create post for individual box page     ####### BOX/USERCREATE
 
   //delete post??                         ###### BOX
 
