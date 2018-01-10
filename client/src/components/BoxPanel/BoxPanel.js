d988a7d64708d7207a5318b9da2ca80744936a55
Michael Chiang <michael.chiang25@gmail.com>
2018-01-10 14:04:46 -0500

    removed MainPanel Component

--- client/src/components/BoxPanel/BoxPanel.js
+++ client/src/components/BoxPanel/BoxPanel.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const BoxPanel = props => (
     <div className="box-panel">
-    	<p>Id:{props.id}</p>
-    	<h2>Box Name: {props.title}</h2>
+    	{/*<p>Id:{props.id}</p>*/}
+    	<h4>Box Name: {props.title}</h4>
     	<p>Description: {props.description}</p>
     	{/*<p>Sentiment Score: {props.sentimentScore}</p>*/}
 	</div>
